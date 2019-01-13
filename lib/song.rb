f093964d64fa253a228a54b63dd83c83e1f231ac
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-13 01:46:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,11 @@ class Song
   attr_accessor :name, :artist, :genre
   @@all = []
   
-  def initialize
-    
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
   end
   
   def self.all
