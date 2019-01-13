22ede9b91f1d54358348356a50c19220c34ffc6d
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-13 02:02:51 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,19 @@ class Patient
   end
   
   def new_appointment(date, doctor)
-    
+    Appointment.new(date, self, doctor)
+  end
+  
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+  
+  def doctors
+    appointments.map do |appoiment|
+      appointment.doctor
+    end
+  end
   
 end
